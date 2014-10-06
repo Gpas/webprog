8169b5c658eafadaa6c16c99b5aaf83c9bb8d43a
Pascal Grueter <pas.grueter@gmail.com>
2014-10-06 09:55:38 +0200

    Navigation mit PHP hinzugefügt, CSS Anpassungen

--- index.php
+++ index.php
@@ -2,38 +2,63 @@
 <html lang="de">
 	<head>
 		<meta charset="utf-8">
-		<link rel="stylesheet" href="css/main.css" >
+		<link rel="stylesheet" href="../css/main.css" >
 		<title>Schoggi Shop</title>
 	</head>
 	<body>
 		<header>
-			<section id="logo">Logo</section>
-			<section id="news">News</section>
+			<img id="logo" src="../resource/logo.jpg" />
+			<div id="slider">Slider</div>
 		</header>
 		<main>
 			<nav>
 			    <ul>
-			    	<?php
-			    		$pages = glob('pages/*');
-						foreach ($pages as $page)
+				    <?php
+			    		$pages = array(
+			    			'Home',
+			    			'Tafeln',
+			    			'Pralinen',
+			    			'Zutaten',
+			    			'Zubehör',
+			    			'Kontakt'
+						);
+						$links= array(
+							'index.php',
+							'tafeln.html',
+							'pralinen.html',
+							'zutaten.html',
+							'zubehör.html',
+							'kontakt.html'
+						);
+						for($i = 0; $i < count($pages); $i++ )
 						{
-							echo '<li><a href="'.$page.'">'.$page.'</a></li>';
+							$nav[$i][0] = $pages[$i];
+							$nav[$i][1] = $links[$i];
+						}
+						foreach($nav as $element){
+							echo '<li><a href="'.$element[1].'">'.$element[0].'</a></li>';
 						}
 			    	?>
 				</ul>
 			</nav>
-			<section id="content">
-				<section id="location">
-					Location
-				</section>
-				Main
-			</section>
 			<aside>
-				<ul>
+                <ul>
                     <li><a href="account.html">Account</a></li>
                     <li><a href="warenkorb.html">Warenkorb</a></li>
                 </ul>
-			</aside>
+            </aside>
+			
+			<article>
+				<section id="location">
+					Location
+				</section>
+				<section id="content" >
+				    Content
+				</section>
+			</article>
+			
 		</main>
+		
+		<footer>Footer</footer>
 	</body>
 </html>
