ec48e27b05583222d84cb1834ed5e65f66ed474c
Pascal Grueter <pas.grueter@gmail.com>
2015-01-01 16:37:36 +0100

    Ordnerstruktur geändert. Pfad heisst nun "site" anstatt "new" und alles alte wurde nach "old" verschoben. Pfad muss in Apache angepasst werden.

---
+++ site/model/Product.class.php
@@ -0,0 +1,86 @@
+<?php
+class Product {
+	private $id;
+	private $category;
+	private $price;
+	private $img;
+	//multilanguage attributes
+	private $name;
+	private $description;
+	
+	//getters
+	public function getId() {
+		return $this->id;
+	}
+	public function getCategory() {
+		return $this->category;
+	}
+	public function getPrice() {
+		return $this->price;
+	}
+	public function getImg() {
+		return $this->img;
+	}
+	public function getName() {
+		return $this->name;
+	}
+	public function getDescription() {
+		return $this->description;
+	}
+	//setters
+	public function setCategory($category) {
+		$this->category = $category;
+	}
+	public function setPrice($price) {
+		$this->price = $price;
+	}
+	public function setImg($img) {
+		$this->img = $img;
+	}
+	public function setName($name) {
+		$this->name = $name;
+	}
+	public function setDescription($description) {
+		$this->description = $description;
+	}
+	
+	public function __toString(){
+		return sprintf("%d %s, %d, %s", $this->id, $this->getName(), $this->price, $this->description);
+	}
+	
+	//functions
+	public static function getProducts($orderBy="id", $lang_code="de"){
+		$orderByStr = '';
+		if (in_array($orderBy, ['id', 'price', 'name', 'description']) ) {
+			$orderByStr = " ORDER BY $orderBy";
+		}
+		$products = array();
+		$res = DB::doQuery("SELECT p.*, l.name AS 'name', l.description AS 'description' FROM products p LEFT OUTER JOIN products_lang l ON p.id = l.product_id WHERE l.lang_code = '$lang_code' $orderByStr");
+		if ($res) {
+			while ($product = $res->fetch_object(get_class())) {
+				$products[] = $product;
+			}
+		}
+		return $products;
+	}
+	
+	public function render(){
+		echo '<article class="product">
+				<h3>'.$this->getName().'</h3>
+				<img alt="Produktbild" src="/assets/images/'.$this->getImg().'">
+				<section class="description">
+					'.$this->getDescription().'
+				</section>
+	 			<form class="orderProduct" method="post" action="/index.php?action=warenkorb">
+					<select name="quantity">';
+		for ($i=1; $i <= 10 ; $i++) { 
+			echo '<option value="'.$i.'">'.$i.'</option>';
+		}
+		echo '</select>
+				<input type="hidden" name="id" value='.$this->getId().' />
+				<button type="submit" name="addProduct" >Bestellen</button>
+				</form>
+				</article>';
+	}
+}
+?>
