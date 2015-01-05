e8ad7bfd0784cf3874933186c76583a9ddc08acc
Dominik Muggli <dominik.muggli@hotmail.com>
2015-01-05 11:45:03 +0100

    Login anpassung

--- site/view/templates/account.php
+++ site/view/templates/account.php
@@ -1,3 +1,6 @@
 
 <h2>Accountdetails</h2>
 <?php echo isset($message) ? "<p>".$message."</p>" : ""; ?>
+	<form action="index.php?action=logout" method="post">
+		<p><input type="submit" value="Ausloggen"></p>
+	</form>
