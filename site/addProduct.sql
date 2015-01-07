6496c1c71c5da35b0576643921235cbdddbb32c6
Pascal Grueter <pas.grueter@gmail.com>
2015-01-07 12:59:05 +0100

    sql update

--- site/addProduct.sql
+++ site/addProduct.sql
@@ -4,11 +4,14 @@ SET @price = 10;
 SET @category = "pralines";
 SET @img = "pralinen.jpg";
 SET @lang = "de";
+SET @options = "1|2|3";
 
-SELECT `AUTO_INCREMENT`
-FROM  INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'web_prog'
-AND   TABLE_NAME   = 'products';
-
-INSERT INTO products (category, price, img) VALUES (@category, @price, @img);
+INSERT INTO products (category, price, img, options) VALUES (@category, @price, @img, @options);
 INSERT INTO products_lang (product_id, lang_code, name ,description) VALUES (last_insert_id(), @lang, @name, @desc);
+
+SET @name = "Option 1";
+SET @values = "80%|90%|100%";
+SET @lang = "de";
+
+INSERT INTO options (opt_values) VALUES (@values);
+INSERT INTO options_lang (options_id, lang_code, name) VALUES (last_insert_id(), @lang, @name);
